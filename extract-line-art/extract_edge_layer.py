7052ed0f175c10b631e499648bd785948dfaddf1
derui <derutakayu@gmail.com>
2017-02-19 09:28:34 +0900

    Fix support commands argument requirements

--- extract-line-art/extract_edge_layer.py
+++ extract-line-art/extract_edge_layer.py
@@ -3,16 +3,16 @@ import argparse
 import numpy as np
 import cv2 as cv
 
-argparser = argparse.ArgumentParser(description='Extract edge layer of a color image')
-argparser.add_argument('image', type=str,
-                       help='the file name of image to extract edge layer')
-argparser.add_argument('-d', dest='out_dir', type=str)
+argparser = argparse.ArgumentParser(
+    description='Extract edge layer of a color image')
+argparser.add_argument(
+    'image',
+    type=str,
+    help='the file name of image to extract edge layer',
+    required=True)
+argparser.add_argument('-d', dest='out_dir', type=str, required=True)
 
 args = argparser.parse_args()
-
-if args.image is None:
-    print("No any files")
-    exit(1)
 
 img = cv.imread(args.image, cv.IMREAD_GRAYSCALE)
 if img is None:
@@ -27,13 +27,10 @@ np.bitwise_not(canny, bitwise)
 
 dirname, fname = os.path.split(os.path.abspath(args.image))
 fname, ext = os.path.splitext(fname)
-if args.out_dir is not None:
-    if not os.path.exists(args.out_dir):
-        os.mkdir(args.out_dir, 0o755)
+if not os.path.exists(args.out_dir):
+    os.mkdir(args.out_dir, 0o755)
 
-    writefname = "%s/%s_converted%s" % (args.out_dir, fname, ext)
-else:
-    writefname = "%s/%s_converted%s" % (dirname, fname, ext)
+writefname = "%s/%s%s" % (args.out_dir, fname, ext)
 
 cv.imwrite(writefname, bitwise)
 print("Wrote image at %s" % (writefname))
--- extract-line-art/resize_fix_size.py
+++ extract-line-art/resize_fix_size.py
@@ -3,16 +3,15 @@ import argparse
 import cv2 as cv
 
 argparser = argparse.ArgumentParser(description='Resize image to fixed size')
-argparser.add_argument('image', type=str,
-                       help='the file name of image to extract edge layer')
-argparser.add_argument('-d', dest='out_dir', type=str)
+argparser.add_argument(
+    'image',
+    type=str,
+    help='the file name of image to extract edge layer',
+    required=True)
+argparser.add_argument('-d', dest='out_dir', type=str, required=True)
 argparser.add_argument('-s', '--size', dest='size', type=int)
 
 args = argparser.parse_args()
-
-if args.image is None:
-    print("No any files")
-    exit(1)
 
 img = cv.imread(args.image)
 if img is None:
@@ -29,6 +28,7 @@ def get_corrected_size(fixed_size, width, height):
         else:
             ratio = fixed_size / w
             return (w * ratio, h * ratio)
+
     size = correct_size(width, height)
     size = correct_size(size[1], size[0])
     return (int(size[1]), int(size[0]))
@@ -43,13 +43,10 @@ img_cropped = img_resized[0:FIXED_SIZE, 0:FIXED_SIZE]
 
 dirname, fname = os.path.split(os.path.abspath(args.image))
 fname, ext = os.path.splitext(fname)
-if args.out_dir is not None:
-    if not os.path.exists(args.out_dir):
-        os.mkdir(args.out_dir, 0o755)
+if not os.path.exists(args.out_dir):
+    os.mkdir(args.out_dir, 0o755)
 
-    writefname = "%s/%s_resized%s" % (args.out_dir, fname, ext)
-else:
-    writefname = "%s/%s_resized%s" % (dirname, fname, ext)
+writefname = "%s/%s%s" % (args.out_dir, fname, ext)
 
 cv.imwrite(writefname, img_cropped)
 print("Wrote image at %s" % (writefname))
