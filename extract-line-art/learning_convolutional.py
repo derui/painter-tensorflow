b774afb9d9bd58bef9ca51bf2c7fa5e2ce98315a
derui <derutakayu@gmail.com>
2017-02-25 09:41:32 +0900

    Add arguments to trace gpu timeline

--- extract-line-art/learning_convolutional.py
+++ extract-line-art/learning_convolutional.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import argparse
 from datetime import datetime
 import numpy as np
 import tensorflow as tf
@@ -12,6 +13,24 @@ BATCH_SIZE = 5
 
 reader = ds.DataSetReader()
 reader.prepare('./datasets')
+
+argparser = argparse.ArgumentParser(
+    description='Learning painter model')
+argparser.add_argument(
+    '--trace_enable',
+    dest='trace_enable',
+    default=False,
+    type=bool,
+    help='Enable or disable tracing')
+argparser.add_argument(
+    '--trace_per_step',
+    dest='trace_per_step',
+    default=100,
+    type=int,
+    help='Per step to get trace. If trace_enable is not set or set false, this option is ignore')
+
+args = argparser.parse_args()
+
 
 with tf.Session() as sess:
 
@@ -37,7 +56,7 @@ with tf.Session() as sess:
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata = tf.RunMetadata()
 
-    for i in range(20000):
+    for i in range(20001):
         batch = reader.read_batch(BATCH_SIZE)
         shape = [BATCH_SIZE, 512, 512, 3]
 
@@ -45,20 +64,30 @@ with tf.Session() as sess:
             x: np.reshape(batch[1], shape),
             y_: np.reshape(batch[0], shape)
         }
-        sess.run(
-            training_op,
-            feed_dict=feed,
-            run_metadata=run_metadata,
-            options=run_options)
 
-        if (i + 1) % 10 == 0:
-            print('step {}, time:{}'.format(i + 1, datetime.utcnow().isoformat()))
+        if args.trace_enable and i > 0 and i % int(args.trace_per_step) == 0:
+            sess.run(
+                training_op,
+                feed_dict=feed,
+                run_metadata=run_metadata,
+                options=run_options)
+        else:
+            sess.run(
+                training_op,
+                feed_dict=feed)
 
-        if (i + 1) % 100 == 0:
+        if i == 0:
+            continue
+
+        if i % 10 == 0:
+            print('step {}, time:{}'.format(i, datetime.utcnow().isoformat()))
+
+        if i % 100 == 0:
             summary_str = sess.run(summary, feed_dict=feed)
             writer.add_summary(summary_str, i)
             saver.save(sess, './log/model.ckpt', i)
 
+        if args.trace_enable and i % int(args.trace_per_step) == 0:
             # write train
             tl = timeline.Timeline(run_metadata.step_stats)
             ctf = tl.generate_chrome_trace_format()
