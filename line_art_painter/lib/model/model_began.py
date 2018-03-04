fix: fix model to append gradients instead of concat

line_art_painter/lib/model/model_began.py
@@ -39,13 +39,13 @@ class Generator(object):
         self.linear = op.LinearEncoder(1024)
 
         self.deconv8 = op.PixelShuffler(op.Encoder(2048, 1024, 3, 3, name='decoder8'), 256, 2)
-        self.deconv7 = op.Encoder(256 + 256, 256, 3, 3, name='decoder7')
+        self.deconv7 = op.Encoder(256, 256, 3, 3, name='decoder7')
         self.deconv6 = op.PixelShuffler(op.Encoder(256 + 256, 512, 3, 3, name='decoder6'), 128, 2)
-        self.deconv5 = op.Encoder(128 + 128, 128, 3, 3, name='decoder5')
+        self.deconv5 = op.Encoder(128, 128, 3, 3, name='decoder5')
         self.deconv4 = op.PixelShuffler(op.Encoder(128 + 128, 256, 3, 3, name='decoder4'), 64, 2)
-        self.deconv3 = op.Encoder(64 + 64, 64, 3, 3, name='decoder3')
+        self.deconv3 = op.Encoder(64, 64, 3, 3, name='decoder3')
         self.deconv2 = op.PixelShuffler(op.Encoder(64 + 64, 128, 3, 3, name='decoder2'), 32, 2)
-        self.deconv1 = op.Encoder(32 + 32, 32, 3, 3, name='decoder1')
+        self.deconv1 = op.Encoder(32, 32, 3, 3, name='decoder1')
         self.deconv0 = op.Encoder(32 + 32, 3, 3, 3, name='decoder0')
 
 
@@ -74,13 +74,13 @@ def generator(image, style):
                          [1, shape[1], shape[2], 1])
 
     deconv8 = relu(gen.bnd8(gen.deconv8(tf.concat([conv8, replicated], 3))))
-    deconv7 = relu(gen.bnd7(gen.deconv7(tf.concat([deconv8, conv7], 3))))
+    deconv7 = relu(gen.bnd7(gen.deconv7(deconv8))) + deconv8
     deconv6 = relu(gen.bnd6(gen.deconv6(tf.concat([deconv7, conv6], 3))))
-    deconv5 = relu(gen.bnd5(gen.deconv5(tf.concat([deconv6, conv5], 3))))
+    deconv5 = relu(gen.bnd5(gen.deconv5(deconv6))) + deconv6
     deconv4 = relu(gen.bnd4(gen.deconv4(tf.concat([deconv5, conv4], 3))))
-    deconv3 = relu(gen.bnd3(gen.deconv3(tf.concat([deconv4, conv3], 3))))
+    deconv3 = relu(gen.bnd3(gen.deconv3(deconv4))) + deconv4
     deconv2 = relu(gen.bnd2(gen.deconv2(tf.concat([deconv3, conv2], 3))))
-    deconv1 = relu(gen.bnd1(gen.deconv1(tf.concat([deconv2, conv1], 3))))
+    deconv1 = relu(gen.bnd1(gen.deconv1(deconv2))) + deconv2
     deconv0 = tf.nn.tanh(gen.deconv0(tf.concat([deconv1, conv0], 3)))
 
     return conv8, deconv7, deconv0
@@ -115,7 +115,7 @@ class GuideDecoder2(object):
         self.bnd2 = op.BatchNormalization(name='guide2_bnd2')
         self.bnd3 = op.BatchNormalization(name='guide2_bnd3')
 
-        self.deconv3 = op.PixelShuffler(op.Encoder(512, 512, 3, 3, name='guide2_decoder3'), 128, 2)
+        self.deconv3 = op.PixelShuffler(op.Encoder(256, 512, 3, 3, name='guide2_decoder3'), 128, 2)
         self.deconv2 = op.PixelShuffler(op.Encoder(128, 256, 3, 3, name='guide2_decoder2'), 64, 2)
         self.deconv1 = op.PixelShuffler(op.Encoder(64, 128, 3, 3, name='guide2_decoder1'), 32, 2)
         self.deconv0 = op.Encoder(32, 3, 3, 3, name="guide2_decoder0")
@@ -183,7 +183,7 @@ def discriminator(img):
     net = relu(D.bnc3_f(D.conv3_f1(net)))
 
     _, h, w, c = net.get_shape().as_list()
-    net = D.fully_connect(net, 128)
+    net = D.fully_connect(net, 256)
     net = D.fully_unconnect(net, h * w * c)
     net = tf.reshape(net, [-1, h, w, c])
 
@@ -215,7 +215,7 @@ def g_loss(gen, gen_pred, original, guide1, guide2, gray_original, alpha=0.3, be
     # where L(v) = |v - D(v)|
     # EBGAN's discriminator as is autoencoder.
 
-    penalty = tf.reduce_mean(tf.abs(gen - original), [1,2,3])
+    penalty = tf.reduce_mean(tf.square(gen - original), [1,2,3])
     g_loss = tf.reduce_mean(tf.abs(gen - gen_pred), [1,2,3])
     g1_loss = alpha * tf.reduce_mean(tf.abs(gray_original - guide1), [1,2,3])
     g2_loss = beta * tf.reduce_mean(tf.abs(original - guide2), [1,2,3])